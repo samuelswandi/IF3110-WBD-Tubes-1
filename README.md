bdc6cb0e298521676644af4cced03fc8d290fbda
Samuel Swandi <samuel.swandi@tokopedia.com>
2022-09-27 18:15:45 +0700

    initial commit

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # WBD-1
+
+## How to run
+```
+docker b
+```
---
+++ index.php
@@ -0,0 +1,6 @@
+<?php
+echo "Hello Grace";
+// atau kalau mau menggunakan variabel untuk menampilkan text maka bisa coba cara dibawah
+// $text = "Hello World";
+// echo $text;
+?>
