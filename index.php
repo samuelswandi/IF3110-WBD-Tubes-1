9f4e7f266e41bb25b7fe692f3eb4684db077b271
Samuel Swandi <samuel.swandi@tokopedia.com>
2022-09-27 19:16:46 +0700

    adding something

--- index.php
+++ index.php
@@ -1,6 +1,46 @@
-<?php
-echo "Hello Grace";
-// atau kalau mau menggunakan variabel untuk menampilkan text maka bisa coba cara dibawah
-// $text = "Hello World";
-// echo $text;
-?>
+<!DOCTYPE html>
+<html>
+    <head>
+        <link rel="stylesheet" href="styles.css"/>
+        <!-- bootstrap purpose -->
+        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+        <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+        <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+    </head>
+
+<body>
+    <div class="main">
+        <div class="login-section">
+            <div class="login-box">
+                <h2>Login</h2>
+                <form class="login-form" role="form" method="post" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>">
+                <div class="form-group row">
+                    <label for="inputEmail" class="col-sm-2 col-form-label">Email</label>
+                    <div class="col-sm-10">
+                    <input type="email" class="form-control" id="inputEmail" name="email" placeholder="Email">
+                    </div>
+                </div>
+                <div class="form-group row">
+                    <label for="inputUser" class="col-sm-2 col-form-label">User Name</label>
+                    <div class="col-sm-10">
+                    <input type="text" class="form-control" id="inputUser" name="user" placeholder="Username">
+                    </div>
+                </div>
+                <div class="form-group row">
+                    <label for="inputPassword3" class="col-sm-2 col-form-label">Password</label>
+                    <div class="col-sm-10">
+                    <input type="password" class="form-control" id="inputPassword" name="password" placeholder="Password">
+                    </div>
+                </div>
+                <div class="form-group row">
+                    <div class="offset-sm-2 col-sm-10">
+                    <input type="submit" value="Sign in" name="submit" class="btn btn-primary"/>
+                    </div>
+                </div>
+                </form>
+            </div>
+        </div>    
+    </div>
+</body>
+</html>
